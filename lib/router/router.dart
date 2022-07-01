d8927eff4996c1040dcc1bbc0ce7c08339329c2b
Wenerson Venâncio <wenersonvenancio@MacBook-Pro-de-Wenerson-2.local>
2022-06-30 22:55:37 -0300

    WeatherUI

---
+++ lib/router/router.dart
@@ -0,0 +1,17 @@
+// import 'package:flutter/material.dart';
+
+// class FirstRouter extends StatelessWidget {
+//   const FirstRouter({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('First Router'),
+//       ),
+//       body: Center(
+//         child: ElevatedButton()
+//       ),
+//     );
+//   }
+// }
